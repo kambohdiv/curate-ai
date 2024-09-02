f7ef3d0fb5aba361e4a7dc2ce3b2100ba4855d2c
Ali Hamza Kamboh <alihamzakamboh180@gmail.com>
2024-09-02 11:17:26 +0500

    fix-signin-up-page

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const SignUpPage = () => {
   return (
     <main className="flex items-center justify-center min-h-screen relative">
- <Link href={'/'} className="absolute top-0 gap-2 left-0 p-6 flex justify-center items-center group">
+ {/* <Link href={'/'} className="absolute top-0 gap-2 left-0 p-6 flex justify-center items-center group">
         <div className="d">
           <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path opacity="0.5" d="M39.9903 0.270225L30.2035 36.8334C30.1538 36.9993 29.9547 37.0491 29.8386 36.9329L23.2864 30.3771L28.1964 12.0541C28.2462 11.8881 28.0969 11.7387 27.931 11.7885L9.61815 16.7012L3.09917 10.1786C2.98306 10.0624 3.03282 9.86327 3.1987 9.81348L39.7249 0.00467392C39.8907 -0.02852 40.04 0.120853 39.9903 0.270225Z" fill="#fa0053" />
@@ -14,7 +14,7 @@ const SignUpPage = () => {
           </svg>
         </div>
         <div className="font-bold text-2xl mt-1 ClashDisplay-Bold group-hover:tracking-wider transition-all duration-300 tracking-wide ">Curate</div>
-      </Link>
+      </Link> */}
       <SignUp />
     </main>
   );
