04e851b36a4bb240a6ab4c386fd5ef233f53a153
Maheen Akhtar Khan <116183925+mkhan429419@users.noreply.github.com>
2024-09-08 23:58:56 +0400

    responsiveness

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -4,7 +4,7 @@ function Dashboard() {
   return (
     <>
       <div className="bg-[#2c2c2c] w-full space-y-5 text-center py-6">
-        <div className="ClashDisplay-Medium text-2xl">What section of your portfolio do you want to enhance today?</div>
+        <div className="ClashDisplay-Medium text-xl sm:text-2xl">What section of your portfolio do you want to enhance today?</div>
         <form className="max-w-2xl mx-auto">
           <label htmlFor="portfolio-search" className="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
           <div className="relative">
@@ -20,43 +20,43 @@ function Dashboard() {
       </div>
 
       <div className="py-8 bg-[#1e1e1e] relative overflow-hidden hide-scroll">
-        <div className="ClashDisplay-Medium text-2xl pl-9">Choose Portfolio Section</div>
-        <div className="flex gap-4 overflow-x-auto w-full p-3 px-9 relative">
-          <div className="bg-gradient-to-r from-[#139EBC] to-[#AA1ECD] p-[2px] min-w-[370px] rounded-xl">
-            <div className="h-[180px] w-full bg-neutral-700 rounded-[10px] p-3 space-y-1">
-              <img src="/portfolio.svg" alt="" />
-              <div className="ClashDisplay-Semibold text-lg tracking-wider">Introduction</div>
-              <div className='ClashDisplay-Reguler text-base'>Craft a compelling introduction to hook potential clients and employers.</div>
+        <div className="ClashDisplay-Medium text-xl sm:text-2xl pl-4 sm:pl-9">Choose Portfolio Section</div>
+        <div className="flex gap-4 overflow-x-auto w-full p-3 sm:px-9 relative">
+          <div className="bg-gradient-to-r from-[#139EBC] to-[#AA1ECD] p-[2px] min-w-[280px] sm:min-w-[370px] rounded-xl">
+            <div className="h-[160px] sm:h-[180px] w-full bg-neutral-700 rounded-[10px] p-3 space-y-1">
+              <img src="/portfolio.svg" alt="Portfolio Icon" />
+              <div className="ClashDisplay-Semibold text-base sm:text-lg tracking-wider">Introduction</div>
+              <div className='ClashDisplay-Reguler text-sm sm:text-base'>Craft a compelling introduction to hook potential clients and employers.</div>
             </div>
           </div>
 
-          <div className="h-[184px] min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
-            <img src="/portfolio.svg" alt="" />
-            <div className="ClashDisplay-Semibold text-lg tracking-wider">Experience</div>
-            <div className='ClashDisplay-Reguler text-base'>Highlight your relevant work experience with AI-powered recommendations for better impact.</div>
+          <div className="h-[160px] sm:h-[180px] min-w-[280px] sm:min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
+            <img src="/portfolio.svg" alt="Portfolio Icon" />
+            <div className="ClashDisplay-Semibold text-base sm:text-lg tracking-wider">Experience</div>
+            <div className='ClashDisplay-Reguler text-sm sm:text-base'>Highlight your relevant work experience with AI-powered recommendations for better impact.</div>
           </div>
 
-          <div className="h-[184px] min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
-            <img src="/portfolio.svg" alt="" />
-            <div className="ClashDisplay-Semibold text-lg tracking-wider">Skills</div>
-            <div className='ClashDisplay-Reguler text-base'>List your top skills, and let AI suggest additional ones that match your industry.</div>
+          <div className="h-[160px] sm:h-[180px] min-w-[280px] sm:min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
+            <img src="/portfolio.svg" alt="Portfolio Icon" />
+            <div className="ClashDisplay-Semibold text-base sm:text-lg tracking-wider">Skills</div>
+            <div className='ClashDisplay-Reguler text-sm sm:text-base'>List your top skills, and let AI suggest additional ones that match your industry.</div>
           </div>
 
-          <div className="h-[184px] min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
-            <img src="/portfolio.svg" alt="" />
-            <div className="ClashDisplay-Semibold text-lg tracking-wider">Projects</div>
-            <div className='ClashDisplay-Reguler text-base'>Showcase your completed projects with suggested descriptions that highlight your achievements.</div>
+          <div className="h-[160px] sm:h-[180px] min-w-[280px] sm:min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
+            <img src="/portfolio.svg" alt="Portfolio Icon" />
+            <div className="ClashDisplay-Semibold text-base sm:text-lg tracking-wider">Projects</div>
+            <div className='ClashDisplay-Reguler text-sm sm:text-base'>Showcase your completed projects with suggested descriptions that highlight your achievements.</div>
           </div>
 
-          <div className="h-[184px] min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
-            <img src="/portfolio.svg" alt="" />
-            <div className="ClashDisplay-Semibold text-lg tracking-wider">Testimonials</div>
-            <div className='ClashDisplay-Reguler text-base'>Gather and display testimonials from previous clients to build trust and credibility.</div>
+          <div className="h-[160px] sm:h-[180px] min-w-[280px] sm:min-w-[374px] bg-neutral-700 rounded-[10px] border-2 border-[#575757] p-3 space-y-1">
+            <img src="/portfolio.svg" alt="Portfolio Icon" />
+            <div className="ClashDisplay-Semibold text-base sm:text-lg tracking-wider">Testimonials</div>
+            <div className='ClashDisplay-Reguler text-sm sm:text-base'>Gather and display testimonials from previous clients to build trust and credibility.</div>
           </div>
         </div>
 
         <div className=''>
-          <div className='ClashDisplay-Medium text-2xl pl-9 pt-9'>Projects</div>
+          <div className='ClashDisplay-Medium text-xl sm:text-2xl pl-4 sm:pl-9 pt-9'>Projects</div>
           <div className="w-full text-center ClashDisplay-Reguler text-base mt-10">You don’t have any projects yet</div>
         </div>
       </div>
